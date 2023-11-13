6827a399d71ed2bf9d768f485f9bac0af2f4ca7c
Omkar Yadav <omkar.sy10@gmail.com>
2023-11-13 02:55:13 -0800

    group api

--- backend/nucleus/urls.py
+++ backend/nucleus/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import UserSignupView, UserLoginView, UserLogoutView
+from .views import UserSignupView, UserLoginView, UserLogoutView, CreateGroupView, DeleteUserView, AddUserToGroupView, RemoveUserFromGroupView
 
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('delete-user/', DeleteUserView.as_view(), name='delete-user'),
+    path('create-group/', CreateGroupView.as_view(), name='create-group'),
+    path('add-user-to-group/', AddUserToGroupView.as_view(), name='add-user-to-group'),
+    path('remove-user-from-group/', RemoveUserFromGroupView.as_view(), name='remove-user-from-group'),
 ]
--- backend/nucleus/views.py
+++ backend/nucleus/views.py
@@ -3,7 +3,12 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSignupSerializer, UserLoginSerializer
+from .serializers import UserSignupSerializer, UserLoginSerializer, GroupSerializer
+from django.contrib.auth import get_user_model
+from .models import Group
+
+User = get_user_model()
+
 
 class UserSignupView(APIView):
     def post(self, request):
@@ -13,11 +18,13 @@ class UserSignupView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class UserLoginView(APIView):
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = authenticate(email=serializer.validated_data['email'], password=serializer.validated_data['password'])
+            user = authenticate(email=serializer.validated_data['email'],
+                                password=serializer.validated_data['password'])
             if user:
                 login(request, user)
                 token, created = Token.objects.get_or_create(user=user)
@@ -25,7 +32,58 @@ class UserLoginView(APIView):
             return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class UserLogoutView(APIView):
     def post(self, request):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
+
+
+class DeleteUserView(APIView):
+    def post(self, request):
+        user_email = request.data.get('user_email')
+        try:
+            user = User.objects.get(email=user_email)
+        except User.DoesNotExist:
+            return Response({'message': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        user.delete()
+        return Response({'message': f'User {user_email} deleted successfully'})
+
+
+class CreateGroupView(APIView):
+    serializer_class = GroupSerializer
+
+    def post(self, request):
+        all_users = []
+        for user_email in request.data.get('members', []):
+            all_users.append(User.objects.get(email=user_email).id)
+        request.data['members'] = all_users
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': f'Group {serializer.data.get("group_name")} Created successfully'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddUserToGroupView(APIView):
+    def post(self, request):
+        group_name = request.data.get('group_name')
+        user_email = request.data.get('user_email')
+        user = User.objects.get(email=user_email)
+        group = Group.objects.get(group_name=group_name)
+        if user not in group.members.all():
+            group.members.add(user.id)
+            return Response({'message': f'User {user_email} successfully added to group {group.group_name}'})
+        return Response({'message': 'User already exists in the group'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RemoveUserFromGroupView(APIView):
+    def post(self, request):
+        group_name = request.data.get('group_name')
+        user_email = request.data.get('user_email')
+        user = User.objects.get(email=user_email)
+        group = Group.objects.get(group_name=group_name)
+        if user in group.members.all():
+            group.members.remove(user.id)
+            return Response({'message': f'User {user_email} successfully removed from group {group.group_name}'})
+        return Response({'message': 'User does not exist in the group'}, status=status.HTTP_400_BAD_REQUEST)

3c1b1bff71bf27d7bda3d77b1ff6d4ede1cbeb0b
Omkar Yadav <omkar.sy10@gmail.com>
2023-11-13 01:58:25 -0800

    backend/nucleus/views.py

--- backend/nucleus/views.py
+++ backend/nucleus/views.py
@@ -6,6 +6,19 @@ from rest_framework import status
 from .serializers import UserSignupSerializer, UserLoginSerializer
 
 class UserSignupView(APIView):
+    """
+    Create a new user with email, first name, last name, and password.
+
+    Request body:
+
+    {
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "password": ""
+    }
+    """
+
     def post(self, request):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
@@ -14,6 +27,15 @@ class UserSignupView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLoginView(APIView):
+    """
+    Login a user with email and password.
+
+    Request body:
+    {
+        "email": ""
+        "password": ""
+    }
+    """
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,6 +48,11 @@ class UserLoginView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLogoutView(APIView):
+    """
+    Logout a user.
+    """
     def post(self, request):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
+
+

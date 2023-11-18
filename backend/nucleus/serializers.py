219e285c9ea3b4dee6c503ac7fff382f9f69953a
Omkar Yadav <omkar.sy10@gmail.com>
2023-11-18 14:19:40 -0800

    bunch of new apis and minor model changes

--- backend/nucleus/serializers.py
+++ backend/nucleus/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Group
+from .models import Group, Chore, Transaction
 
 User = get_user_model()
 
@@ -26,6 +26,66 @@ class UserLoginSerializer(serializers.Serializer):
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    # members = serializers.PrimaryKeyRelatedField(
+    #     queryset=User.objects.all()
+    # )
+    # members = serializers.ListField(
+    #     child=serializers.EmailField()
+    # )
+
     class Meta:
         model = Group
+        fields = ['id', 'group_name', 'members', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+
+
+class ChoreSerializer(serializers.ModelSerializer):
+    assigned_to = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='email',
+        required=False,
+        allow_null=True
+    )
+    group = serializers.PrimaryKeyRelatedField(
+        queryset=Group.objects.all()
+    )
+
+    class Meta:
+        model = Chore
+        fields = ['id', 'name', 'description', 'karma_value', 'group', 'assigned_to', 'is_recurring', 'created_at', 'due_date', 'completed_at', 'status']
+        read_only_fields = ['id', 'created_at']
+
+    def create(self, validated_data):
+        return Chore.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.karma_value = validated_data.get('karma_value', instance.karma_value)
+        instance.group = validated_data.get('group', instance.group)
+        instance.assigned_to = validated_data.get('assigned_to', instance.assigned_to)
+        instance.is_recurring = validated_data.get('is_recurring', instance.is_recurring)
+        instance.due_date = validated_data.get('due_date', instance.due_date)
+        instance.completed_at = validated_data.get('completed_at', instance.completed_at)
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+        return instance
+
+# class TransactionSerializer(serializers.Serializer):
+#     # from_user = serializers.SlugRelatedField(
+#     #     queryset=User.objects.all(),
+#     #     slug_field='email'
+#     # )
+#     # to_user = serializers.SlugRelatedField(
+#     #     queryset=User.objects.all(),
+#     #     slug_field='email'
+#     # )
+#     karma = serializers.DecimalField(max_digits=10, decimal_places=2)
+#     created_at = serializers.DateTimeField(read_only=True)
+
+
+class TransactionSerializer(serializers.Serializer):
+    class Meta:
+        model = Transaction
         fields = '__all__'
--- backend/nucleus/views.py
+++ backend/nucleus/views.py
@@ -3,9 +3,13 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSignupSerializer, UserLoginSerializer, GroupSerializer
+from .serializers import UserSignupSerializer, UserLoginSerializer, GroupSerializer, ChoreSerializer, \
+    TransactionSerializer
 from django.contrib.auth import get_user_model
-from .models import Group
+from .models import Group, Chore, Transaction, Debt, TransactionParticipant
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
 
@@ -42,6 +46,7 @@ class UserLoginView(APIView):
         "password": ""
     }
     """
+
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
@@ -59,6 +64,7 @@ class UserLogoutView(APIView):
     """
     Logout a user.
     """
+
     def post(self, request):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
@@ -75,32 +81,52 @@ class DeleteUserView(APIView):
         return Response({'message': f'User {user_email} deleted successfully'})
 
 
-class CreateGroupView(APIView):
-    serializer_class = GroupSerializer
+#
+# class CreateGroupView(APIView):
+#     serializer_class = GroupSerializer
+#
+#     def post(self, request):
+#         user_email = request.data.get('user_email')
+#         request.data['members'] = [User.objects.get(email=user_email).id]
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'message': f'Group {serializer.data.get("group_name")} Created successfully'})
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ViewUserGroupsView(APIView):
+#     def get(self, request):
+#         user_email = request.data.get('user_email')
+#         user = User.objects.get(email=user_email)
+#         groups = Group.objects.filter(members__in=[user.id])
+#         serializer = GroupSerializer(groups, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
 
-    def post(self, request):
-        all_users = []
-        for user_email in request.data.get('members', []):
-            all_users.append(User.objects.get(email=user_email).id)
-        request.data['members'] = all_users
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'message': f'Group {serializer.data.get("group_name")} Created successfully'})
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class AddUserToGroupView(APIView):
+#     def post(self, request):
+#         group_name = request.data.get('group_name')
+#         user_email = request.data.get('user_email')
+#         user = User.objects.get(email=user_email)
+#         group = Group.objects.get(group_name=group_name)
+#         if user not in group.members.all():
+#             group.members.add(user.id)
+#             return Response({'message': f'User {user_email} successfully added to group {group.group_name}'})
+#         return Response({'message': 'User already exists in the group'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AddUserToGroupView(APIView):
-    def post(self, request):
-        group_name = request.data.get('group_name')
-        user_email = request.data.get('user_email')
-        user = User.objects.get(email=user_email)
-        group = Group.objects.get(group_name=group_name)
-        if user not in group.members.all():
-            group.members.add(user.id)
-            return Response({'message': f'User {user_email} successfully added to group {group.group_name}'})
-        return Response({'message': 'User already exists in the group'}, status=status.HTTP_400_BAD_REQUEST)
-
+# class BulkAddUsersToGroupView(APIView):
+#     def post(self, request):
+#         group_name = request.data.get('group_name')
+#         user_emails = request.data.get('user_emails')
+#         group = Group.objects.get(group_name=group_name)
+#         for user_email in user_emails:
+#             user = User.objects.get(email=user_email)
+#             if user not in group.members.all():
+#                 group.members.add(user.id)
+#         return Response({'message': f'Users {user_emails} successfully added to group {group.group_name}'})
+#
 
 class RemoveUserFromGroupView(APIView):
     def post(self, request):
@@ -112,3 +138,207 @@ class RemoveUserFromGroupView(APIView):
             group.members.remove(user.id)
             return Response({'message': f'User {user_email} successfully removed from group {group.group_name}'})
         return Response({'message': 'User does not exist in the group'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# ------------ GROUPS ------------------
+class CreateGroupView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response({'message': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        group_data = {
+            'group_name': request.data.get('group_name'),  # You can modify this based on your request data
+            'members': [str(request.user.id)],  # Add the current user to the members list
+        }
+
+        serializer = GroupSerializer(data=group_data)
+        if serializer.is_valid():
+            group = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ListGroupsView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Ensure the user is authenticated
+        if not request.user.is_authenticated:
+            return Response({'message': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Filter groups where the user is a member
+        groups = Group.objects.filter(members__id=request.user.id)
+        serializer = GroupSerializer(groups, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GroupDetailView(APIView):
+    def get(self, request, group_id):
+        try:
+            group = Group.objects.get(id=group_id)
+            serializer = GroupSerializer(group)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Group.DoesNotExist:
+            return Response({'message': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UpdateGroupView(APIView):
+    """
+    Give group name and member email IDs to add members to a group. Also group ID in url.
+    """
+
+    def put(self, request, group_id):
+        group = Group.objects.get(id=group_id)
+        serializer = GroupSerializer(group, data=request.data)
+        member_emails = request.data.get('members')
+        members = []
+        if member_emails:
+            for member_email in member_emails:
+                member = User.objects.get(email=member_email)
+                members.append(member.id)
+        serializer.initial_data['members'] = members
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class BulkAddUsersToGroupView(APIView):
+#     def post(self, request):
+#         group_id = request.data.get('group_id')
+#         user_emails = request.data.get('user_emails')
+#         group = Group.objects.get(group_id=group_id)
+#         for user_email in user_emails:
+#             user = User.objects.get(email=user_email)
+#             if user not in group.members.all():
+#                 group.members.add(user.id)
+#         return Response({'message': f'Users {user_emails} successfully added to group {group.group_name}'})
+
+
+class DeleteGroupView(APIView):
+    def delete(self, request, group_id):
+        group = Group.objects.get(id=group_id)
+        group.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# ------------------ CHORES ------------------
+class ListChoresView(APIView):
+    """
+    Send group id and get list of chores for that group.
+    """
+
+    def get(self, request, group_id):
+        try:
+            group = Group.objects.get(id=group_id)
+            chores = Chore.objects.filter(group=group)
+            serializer = ChoreSerializer(chores, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Group.DoesNotExist:
+            return Response({'message': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class CreateChoreView(APIView):
+    def post(self, request):
+        serializer = ChoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChoreDetailView(APIView):
+    def get(self, request, chore_id):
+        try:
+            chore = Chore.objects.get(id=chore_id)
+            serializer = ChoreSerializer(chore)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Chore.DoesNotExist:
+            return Response({'message': 'Chore not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UpdateChoreView(APIView):
+    def put(self, request, chore_id):
+        chore = Chore.objects.get(id=chore_id)
+        serializer = ChoreSerializer(chore, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteChoreView(APIView):
+    def delete(self, request, chore_id):
+        chore = Chore.objects.get(id=chore_id)
+        chore.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class AssignChoreView(APIView):
+    def post(self, request):
+        chore_id = request.data.get('chore_id')
+        chore = Chore.objects.get(id=chore_id)
+        user_id = request.data.get('user_id')
+        user = User.objects.get(id=user_id)
+        chore.assigned_to = user
+        chore.save()
+        return Response({'message': f'Chore {chore.name} assigned to {user.get_full_name()}'})
+
+
+# # ------------------ TRANSACTIONS ------------------
+
+class CreateTransactionView(APIView):
+    """
+    This view is basically for DO THE TASK.
+    """
+    serializer_class = TransactionSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response({'message': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+        done_by = request.user
+        group = request.data.get('group')
+        try:
+            group = Group.objects.get(id=group)
+        except Group.DoesNotExist:
+            return Response({'message': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
+        name = request.data.get('name')
+        description = request.data.get('description')
+        users_involved = request.data.get('users_involved')
+        try:
+            users_involved = [User.objects.get(id=user) for user in users_involved]
+        except User.DoesNotExist:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        chore = request.data.get('chore')
+        try:
+            chore = Chore.objects.get(id=chore)
+        except Chore.DoesNotExist:
+            return Response({'message': 'Chore not found'}, status=status.HTTP_404_NOT_FOUND)
+        karma_value = chore.karma_value
+        per_member_share = karma_value / len(users_involved)
+
+        settlements = []
+        transaction = Transaction.objects.create(name=name, description=description, group=group,
+                                                 karma_value=karma_value, done_by=done_by, chore=chore)
+
+        for user in users_involved:
+            if user != done_by:
+                debt = Debt.objects.create(from_user=done_by, to_user=user, karma=per_member_share)
+                group.debts.add(debt)
+                settlements.append(debt)
+            transaction_participant = TransactionParticipant.objects.create(user=user, transaction=transaction,
+                                                                            karma_earned=(
+                                                                                0 if user != done_by else per_member_share),
+                                                                            karma_owed=per_member_share, net_balance=(-per_member_share if user != done_by else karma_value - per_member_share))
+
+        transaction.settlements.set(settlements)
+        transaction.users_involved.set(users_involved)
+        transaction.save()
+        return Response({'message': 'Expense Created successfully'})

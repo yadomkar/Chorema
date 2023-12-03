3480d0c07dd874cdbab5792ecdef6f151bdc0b28
Pruthviraj Urankar <pruthvirajurankar@yahoo.in>
2023-12-02 20:16:15 -0800

    added create group button on home page

--- frontend/app/routes/dashboard/Dashboard.tsx
+++ frontend/app/routes/dashboard/Dashboard.tsx
@@ -97,81 +97,79 @@ export function Component(): JSX.Element {
   }
 
   return (
-    <Container sx={{ py: "20px" }} maxWidth="sm">
-      <Typography sx={{ mb: 2 }} variant="h3" align="center">
-        Welcome {user?.first_name ?? "to Chorema"}
-      </Typography>
-
-      <Divider />
-      <br />
-
-      <Box
-        sx={{
-          justifyContent: "center",
-          display: "flex",
-        }}
-      >
-        <Button variant="outlined" onClick={onCreateGroup}>
-          Create new group
-        </Button>
-      </Box>
-
-      <br />
-
-      <Box
-        sx={{
-          justifyContent: "center",
-          gridGap: "1rem",
-        }}
-      >
-        <Typography sx={{ my: 2 }} variant="h4" align="center">
-          Your Groups
+    <>
+      <Container sx={{ py: "20px" }} maxWidth="sm">
+        <Typography sx={{ mb: 2 }} variant="h3" align="center">
+          Welcome {user?.first_name ?? "to Chorema"}
         </Typography>
+
+        <Divider />
 
         <br />
 
-        <List sx={{ width: "100%" }}>
-          {groups?.map((group) => {
-            return (
-              <>
-                <ListItem
-                  secondaryAction={
+        <Box
+          sx={{
+            justifyContent: "center",
+            gridGap: "1rem",
+          }}
+        >
+          <Typography sx={{ my: 2 }} variant="h4" align="center">
+            Your Groups
+          </Typography>
+          <br />
+
+          <List sx={{ width: '100%' }}>
+            {groups?.map((group) => {
+              return (
+                <>
+                  <ListItem secondaryAction={
                     <>
                       <IconButton edge="end" aria-label="edit">
-                        <Create
-                          onClick={() => {
-                            navigate(`/group/edit/${group?.id}`);
-                          }}
-                        />
+                        <Create onClick={() => { navigate(`/group/edit/${group?.id}`) }} />
                       </IconButton>
                       <IconButton edge="end" aria-label="edit" sx={{ ml: 1 }}>
-                        <Delete onClick={() => {}} />
+                        <Delete onClick={() => { }} />
                       </IconButton>
                     </>
                   }
-                  sx={{
-                    border: "0.8px solid grey",
-                    borderRadius: 5,
-                    background: "#d6f5f5",
-                  }}
-                >
-                  <ListItemButton sx={{ padding: 0 }}>
-                    <ListItemText
-                      primary={group.group_name}
-                      onClick={() => {
-                        navigate(`/group/view/${group?.id}`);
-                      }}
-                      secondary={`${group.members?.length ?? 0} members`}
-                    />
-                  </ListItemButton>
-                </ListItem>
-                <Divider sx={{ my: 2 }} />
-              </>
-            );
-          })}
-        </List>
-      </Box>
-    </Container>
+                    sx={{
+                      border: '0.8px solid grey', borderRadius: 5, background: '#d6f5f5'
+                    }}
+                  >
+                    <ListItemButton sx={{ padding: 0 }}>
+                      <ListItemText primary={group.group_name} onClick={() => { }} secondary={`${group.members?.length ?? 0} members`} />
+                    </ListItemButton>
+
+                  </ListItem>
+                  <Divider sx={{ my: 2 }} />
+                </>
+              );
+            })}
+
+          </List>
+        </Box>
+
+
+      </Container >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "fixed",
+          width: "100%",
+          bottom: "40px",
+        }}
+      >
+        <Button
+          children="Create Group"
+          variant="outlined"
+          onClick={onCreateGroup}
+          fullWidth
+          sx={{ mx: 4 }}
+        />
+      </div>
+    </>
   );
 }
 

737eea25a882c14d8d8a188019801394ce2c3b57
Pruthviraj Urankar <pruthvirajurankar@yahoo.in>
2023-12-02 20:26:21 -0800

    code refactor

--- frontend/app/core/auth.ts
+++ frontend/app/core/auth.ts
@@ -1,13 +1,11 @@
 import { type User, type UserCredential } from "firebase/auth";
 import * as React from "react";
 import { atom } from "recoil";
-import { useOpenLoginDialog } from "../dialogs/LoginDialog.js";
 import { UserDetailObject } from "../routes/auth/Login.hooks.js";
 import {
   auth,
-  signIn,
   type SignInMethod,
-  type SignInOptions,
+  type SignInOptions
 } from "./firebase.js";
 
 let idTokenPromise: Promise<string | null> | undefined;
@@ -72,25 +70,6 @@ export function useCurrentUser() {
   return token && session_id ? user : undefined;
 }
 
-
-export function useSignIn() {
-  const openLoginDialog = useOpenLoginDialog();
-  return React.useCallback(
-    async function (options?: SignInOptions) {
-      if (options?.method) {
-        try {
-          return await signIn(options);
-        } catch (err) {
-          return await openLoginDialog({ error: err as Error });
-        }
-      } else {
-        return await openLoginDialog();
-      }
-    },
-    [openLoginDialog],
-  );
-}
-
 export const signOut = () => {
   return new Promise((resolve) => {
     localStorage.clear();
@@ -101,67 +80,6 @@ export const signOut = () => {
 export function useSignOut() {
   return React.useCallback(() => signOut(), []);
 }
-
-/**
- * Returns a memoized version of the callback that triggers opening a login
- * dialog in case the user is not authenticated.
- *
- * @example
- *    const saveProfile = useAuthCallback(async (me) => {
- *      await updateProfile(me, input);
- *    }, [input])
- */
-export function useAuthCallback<T extends AuthCallback>(
-  callback: T,
-  deps: React.DependencyList = [],
-): (...args: AuthCallbackParameters<T>) => Promise<Awaited<ReturnType<T>>> {
-  const openLoginDialog = useOpenLoginDialog();
-  return React.useCallback(
-    async (...args: AuthCallbackParameters<T>) => {
-      const fb = await import("../core/firebase.js");
-
-      try {
-        if (!fb.auth.currentUser) {
-          await openLoginDialog();
-        }
-
-        if (!fb.auth.currentUser) {
-          return Promise.reject(new Error("Not authenticated."));
-        }
-
-        return await callback(fb.auth.currentUser, ...args);
-      } catch (err) {
-        const code = (err as { code?: string })?.code;
-
-        // https://firebase.google.com/docs/reference/js/auth
-        if (code?.startsWith?.("/auth") || code === "permission-denied") {
-          await openLoginDialog();
-
-          if (!fb.auth.currentUser) {
-            throw new Error("Not authenticated.");
-          }
-
-          return await callback(fb.auth.currentUser, ...args);
-        } else {
-          throw err;
-        }
-      }
-    },
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-    [openLoginDialog, ...deps],
-  );
-}
-
-type AuthCallbackParameters<T extends AuthCallback> = Parameters<T> extends [
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  infer _,
-  ...infer Tail,
-]
-  ? Tail
-  : never;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AuthCallback = (user: User, ...args: any) => any;
 
 export {
   type SignInMethod,
